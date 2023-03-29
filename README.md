6304283aca75565878aa6536a7e330c6643bf149
Danilo <daanrod93@gmail.com>
2023-03-28 22:07:15 -0300

    Output atualizado para ser exibido somente em 1 linha.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pixel_color_bot
-Print RGB and HEX color values, made for get colors when I was studying HTML/CSS.
+
+Print RGB and HEX color values from mouse position.
--- pixel.py
+++ pixel.py
@@ -2,6 +2,8 @@ import time
 
 import pyautogui
 
+LINE_CLEAR = '\x1b[2K'
+
 while True:
     try:
         posXY = pyautogui.position()
@@ -10,10 +12,11 @@ while True:
         to_str = str(rgb).strip("()")
         print(
             f"RGB: {to_str} | "
-            + 
-             "HEX: #%02X%02X%02X" % (to_list[0], to_list[1], to_list[2])
+            +
+            "HEX: #%02X%02X%02X" % (to_list[0], to_list[1], to_list[2]),
+            end="\r"
         )
-        time.sleep(0.5)
+        print(end=LINE_CLEAR)
     except Exception as e:
         print(e)
     except KeyboardInterrupt:
